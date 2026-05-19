c31bafb8583044610b06ed9f844d33e2b3d36c0a
Your Name <your.email@example.com>
2026-05-20 01:50:36 +0300

    fix: use default .vercel output for start script

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig } from '@tanstack/start/config'
+import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  server: {
-    preset: 'node-server',   // ✅ this tells Start to build for Node.js (outputs .output/server/index.mjs)
-  },
-  vite: {
-    plugins: [
-      viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
-      tailwindcss(),
-      viteReact(),
-    ],
-  },
+  plugins: [
+    viteTsConfigPaths({ projects: ['./tsconfig.json'] }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
 })
